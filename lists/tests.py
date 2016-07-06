6082314a4bf484c255fa0d89d876d4a59264f544
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2016-07-06 16:19:35 +0500

    Add inputbox and empty list table

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
@@ -19,7 +20,10 @@ class HomePageTest(TestCase):
         response = home_page(request)
         self.assertTrue(response.content.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>'))
+        self.assertTrue(response.content.strip().endswith('</html>'))
+
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content, expected_html)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
